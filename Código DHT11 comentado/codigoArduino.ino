90755a5c8ad9c1b361c1581adde9b68ae22f50cb
AlbaDr52 <pablo.santana@sptech.school>
2024-04-17 20:45:57 -0300

    codigo dht11 atualizado

---
+++ Código DHT11 comentado/codigoArduino.ino
@@ -0,0 +1,29 @@
+#include "DHT.h"
+#define dht_type DHT11
+
+int dht_pin = A1;
+DHT dht_1 = DHT(dht_pin, dht_type); // definição de variaveis
+
+void setup() { // codigo que roda uma vez só
+  Serial.begin(9600);
+  dht_1.begin();
+}
+
+void loop() { // codigo que roda diversas vezes
+  float umidade = dht_1.readHumidity(); // variavel de leitura - umidade
+  umidade = umidade * 1; // simulação de umidade
+  float temperatura = dht_1.readTemperature(); // variavel de leitura - temperatura
+  temperatura = temperatura * 1; // simulação de temperatura
+  if(isnan(temperatura) or isnan(umidade)){
+    Serial.println("Erro ao ler");  // caso haja erro de leitura, printa na tela
+  } else {
+    // Serial.print("Umidade: ");
+    Serial.print(umidade);
+    Serial.print(";"); // 
+    // Serial.print(" % ");
+   // Serial.print(" Temperatura: ");
+    Serial.println(temperatura);
+    // Serial.println(" ºC");
+  }
+  delay(500);
+}
