547d398b866d3eb1d7a6e12899485b5cc8b07693
GustavoBasi <gustavo.bcruz@sptech.school>
2024-05-01 09:59:58 -0300

    Comentários no código do arduino

--- Código DHT11 comentado/codigoArduino/codigoArduino.ino
+++ Código DHT11 comentado/codigoArduino/codigoArduino.ino
@@ -1,43 +1,43 @@
-#include "DHT.h"
-#define dht_type DHT11
+#include "DHT.h" // biblioteca do dht
+#define dht_type DHT11 // definição de tipo de dht, ou seja, dht11
 
 int dht_pin = A1;
 DHT dht_1 = DHT(dht_pin, dht_type); // definição de variaveis
 
 void setup() { // codigo que roda uma vez só
-  Serial.begin(9600);
+  Serial.begin(9600); // velocidade do monitor serial
   dht_1.begin();
 }
 
 void loop() { // codigo que roda diversas vezes
   float umidade = dht_1.readHumidity(); // variavel de leitura - umidade
   float temperatura = dht_1.readTemperature(); // variavel de leitura - temperatura
-  const FK = 1;
-  float umidade2 = umidade * 1.1;
-  float temperatura2 = temperatura * 1.1;
-  const FK2 = 2;
-  float umidade3 = umidade * 0.9;
-  float temperatura3 = temperatura * 0.9;
-  const FK3 = 3;
+  const FK = 1; // variavel da foreign key do banco de dados  
+  float umidade2 = umidade * 1.1; // variavel de simulação da umidade
+  float temperatura2 = temperatura * 1.1; // variavel de simulação da temperatura
+  const FK2 = 2; // variavel da foreign key do banco de dados de exemplo
+  float umidade3 = umidade * 0.9; // variavel de simulação da umidade
+  float temperatura3 = temperatura * 0.9; // variavel de simulação da temperatura
+  const FK3 = 3; // variavel da foreign key do banco de dados de exemplo
+
   if(isnan(temperatura) or isnan(umidade)){
     Serial.println("Erro ao ler");  // caso haja erro de leitura, printa na tela
   } else {
-    Serial.print(umidade);
-    Serial.print(";"); // 
-    Serial.print(temperatura);
-    Serial.print(";");
-    Serial.println(FK);
-    //Serial.println(";");
-    Serial.print(umidade2);
-    Serial.print(";"); // 
-    Serial.print(temperatura2);
-    Serial.print(";");
-    Serial.println(FK2);
-    Serial.print(umidade3);
-    Serial.print(";"); // 
-    Serial.print(temperatura3);
-    Serial.print(";");
-    Serial.println(FK3);
+    Serial.print(umidade); // valor de umidade captados pelo sensor
+    Serial.print(";"); // separação dos valores para interpretação da api
+    Serial.print(temperatura); // valor de temperatura captados pelo sensor
+    Serial.print(";"); // separação dos valores para interpretação da api
+    Serial.println(FK); // Foreign key do banco de dados, da tabela do sensor
+    Serial.print(umidade2); // valor de umidade captados pelo sensor de exemplo
+    Serial.print(";"); // separação dos valores para interpretação da api
+    Serial.print(temperatura2); // valor de temperatura captados pelo sensor de exemplo
+    Serial.print(";"); // separação dos valores para interpretação da api
+    Serial.println(FK2); // Foreign key do banco de dados, da tabela do sensor de exemplo
+    Serial.print(umidade3); // valor de umidade captados pelo sensor de exemplo
+    Serial.print(";"); // separação dos valores para interpretação da api
+    Serial.print(temperatura3); // valor de temperatura captados pelo sensor de exemplo
+    Serial.print(";"); // separação dos valores para interpretação da api
+    Serial.println(FK3); // Foreign key do banco de dados, da tabela do sensor de exemplo
   }
-  delay(5000);
+  delay(5000); // delay de 5 segundos para cada leitura
 }
