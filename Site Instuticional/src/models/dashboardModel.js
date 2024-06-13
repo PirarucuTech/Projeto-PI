f9029377fc2e503ab16ee99e63335ff1ac2650bc
AlbaDr52 <pablo.santana@sptech.school>
2024-06-13 19:58:37 -0300

    detalhes dash de visão unica

--- Site Instuticional/src/models/dashboardModel.js
+++ Site Instuticional/src/models/dashboardModel.js
@@ -12,7 +12,11 @@ ON tanque.id = sensor.fkTanque AND tanque.fkEmpresa = sensor.fkTanqueEmpresa
 LEFT JOIN leitura
 ON sensor.id = leitura.fkSensor
 WHERE tanque.fkEmpresa = ${idEmpresa}
-AND leitura.diahora IN ( SELECT MAX(leitura.diahora) FROM tanque JOIN sensor ON tanque.id = sensor.fkTanque AND tanque.fkEmpresa = sensor.fkTanqueEmpresa LEFT JOIN leitura ON sensor.id = leitura.fkSensor GROUP BY tanque.identificacao);
+AND leitura.diahora IN ( SELECT MAX(leitura.diahora) 
+FROM tanque 
+JOIN sensor ON tanque.id = sensor.fkTanque AND tanque.fkEmpresa = sensor.fkTanqueEmpresa 
+LEFT JOIN leitura ON sensor.id = leitura.fkSensor 
+GROUP BY tanque.identificacao);
     `;
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
